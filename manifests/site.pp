a94acf7bc9a0f0e0abf20cb11a8554f49eefb050
pauloquinete <43891466+pauloquinete@users.noreply.github.com>
2022-10-20 22:16:30 -0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,12 +1,10 @@
 node default {
-  file {'/root/REAMDE':
-    ensure  => file,
-    content => 'This is a readme',
-    owner   => 'root',
-  }
 }
 node 'master.puppet.vm' {
   include role::master_server
+  file {'/root/README':
+    ensure  => file,
+    content => $fqdn,
 }
 
 node /^web/ {
