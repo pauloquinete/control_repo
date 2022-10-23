0dec203aec3d685002701c179d2cf04438e99bca
pauloquinete <43891466+pauloquinete@users.noreply.github.com>
2022-10-23 17:08:55 -0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -7,11 +7,12 @@ node 'master.puppet.vm' {
     content => "Welcome to ${fqdn} \n" ,
   }
 }
-
+node 'minetest.puppet.vm {
+  include role::minecraft_server
+}
 node /^web/ {
   include role::app_server
 }
-
 node /^db/ {
   include role::db_server
 }
