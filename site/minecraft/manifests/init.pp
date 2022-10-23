bb8a784a73f283ce0fcad002365f6f95d4347d8f
pauloquinete <43891466+pauloquinete@users.noreply.github.com>
2022-10-23 19:18:35 -0300

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,6 +1,7 @@
 class minecraft (
   $url = 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.1/minecraft_server.1.12.1.jar',
-  $install_dir = '/opt/minecraft'
+  $install_dir = '/opt/minecraft',
+  $java_package = 'https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.rpm'
 ){
   file { $install_dir:
     ensure => directory,
@@ -9,9 +10,10 @@ class minecraft (
     ensure => file,
     source => $url,
   }
-
   package {'java':
     ensure => present,
+    source => $java_package,
+    provider => 'rpm',
   }
   file {"${install_dir}/eula.txt":
     ensure => file,
